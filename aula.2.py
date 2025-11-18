0048f67bd3bdf490b950efb5f3011a2beaf21226
Elton-morais12 <eltonsouzademorais@gmail.com>
2025-11-18 19:40:56 -0300

    Inicia o repositório da aula 02

---
+++ aula.2.py
@@ -0,0 +1,48 @@
+# n1 = int(input('informe o primeiro numero'))
+
+# x = 5
+# x = '10'
+# x = 100
+# print(x)
+
+
+# nome_usuario = input('informe o nome:')
+# idade_usuario = input('informe a idade: ')
+# print(f'O nome é{nome_usuario} e sua idade é {idade_usuario}')
+
+
+
+# cidade = input('Informe a sua cidade: ')
+# bairro = input('Informe o seu bairro: ')
+# print(f'Sua cidade é {cidade} e seu bairro é {bairro}')
+
+# num1 = float(input('informe o número: '))
+# num2 = float(input('Informe o segundo número:'))
+# resultado = num1 * num2
+# print(resultado)
+
+# idade = int(input('qual a sua idade' ))
+# ano = 2025
+# resultado = ano - idade
+# print(f'O ano de nascimento é {resultado}')
+
+# dinheiro = int(input('quanto em dinheiro você possui ?'))
+# ingresso = 30
+# print('O valor do ingresso é 7')
+# resultado = dinheiro / ingresso
+# print(f'você possui {resultado}' )
+
+# preço = float(input(f'informe o preço: '))
+# qtd = int(input('informe a quantidade: '))
+# total = preço * qtd
+# desconto = total * 0.1
+# valor_final = total - desconto
+# print(f'valor total {total}\nDesconto de 10% {desconto}\nValor a pagar:{valor_final:.2f}')
+
+# salario = int(input('valor do seu salario?'))
+# aumento = salario * 20/100
+# print(f'seu salario atual é {salario} o aumento é {aumento}o salario atual {aumento + salario}')
+
+#ler numero de usuario 
+
+# numero =int(input('Digite um numero'))
